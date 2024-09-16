314f0f7da26a4f1eb5913a388245d7045f3bdc75
MohammedKamalAlsyd <MohammedKamalAlsyd@gmail.com>
2024-09-16 04:09:35 +0300

    Add Manifest to Vite Configuration

--- TimJS/vite.config.js
+++ TimJS/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import jsconfigPaths from "vite-jsconfig-paths";
+import { crx } from "@crxjs/vite-plugin";
+import manifestJson from "./public/manifest.json";
+
+const manifest = manifestJson;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),crx({ manifest }), jsconfigPaths()],
 })
